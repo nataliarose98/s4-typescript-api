6be69f99f97031b64e3e488563e652c2bacf6054
Natalia <natarose727@gmail.com>
2025-11-26 13:02:58 +0100

    feat: exercise 1 - fetch and display jokes from API with button

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,27 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <div>
-    <h1>Jokes app</h1>
+    <h1>Jokes App</h1>
     <div id="joke-container"></div>
-    <button id="next-joke">Next joke</button>
+    <button id="next-joke">Next Joke</button>
   </div>
 `;
+
+const jokeContainer = document.querySelector<HTMLDivElement>('#joke-container')!
+const nextJokeBtn = document.querySelector<HTMLButtonElement>('#next-joke')!
+
+async function fetchJoke(): Promise<string> {
+  const response = await fetch('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json'}})
+  const data = await response.json()
+  return data.joke
+}
+
+async function showJoke() {
+  const joke = await fetchJoke()
+  jokeContainer.textContent = joke
+  console.log(joke)
+}
+
+showJoke()
+
+nextJokeBtn.addEventListener('click', showJoke)
